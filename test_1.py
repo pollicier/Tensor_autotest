9fd52060374fe83ce5d5a0ee63514e8f3c8c0ae1
pollicier <pollicier@gmail.com>
2020-03-31 16:25:30 +0300

    Add functions, bug fix

--- test_1.py
+++ test_1.py
@@ -4,14 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+# задаём все переменные, используемые в коде
+link = "https://yandex.ru/"
+search_selector = ".input__control.input__input"
+table_selector = "div.mini-suggest__popup-content"
+find_button_selector = ".button.mini-suggest__button"
+first_link_selector = "#search-result > li:nth-child(3) > div > h2 > a"
+request = "тензор"
+compared_value = "https://tensor.ru/"
+
 # инициализируем драйвер браузера
 browser = webdriver.Chrome()
 browser.maximize_window()
+browser.implicitly_wait(15)
 
-# задаём через переменную нужную ссылку и открываем её
-link = "https://yandex.ru/"
-browser.get(link)
-browser.implicitly_wait(5)
+
+# помещаем тестовые сценарии в функции
 
 
 def do_action(target):
@@ -26,39 +34,47 @@ def do_action(target):
     return target_element
 
 
-# чтобы гарантировать корректную работу, используем конструкцию try/finally
+class TestMainPage(object):
+
+    def __init__(self):
+        pass
+
+    def test_find_search(self, selector, query):
+        browser.get(link)
+        search = WebDriverWait(browser, 10).until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, selector))
+        )
+        ActionChains(browser).move_to_element(search).perform()
+        assert search, "Поле не найдено"
+        search.send_keys(query)
+
+    def test_table(self, selector):
+        table = WebDriverWait(browser, 10).until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, table_selector))
+        )
+        assert table, "Таблица не найдена"
+
+    def test_first_link(self, selector, value_for_comparison):
+        first_link_target = WebDriverWait(browser, 10).until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, selector))
+        )
+        first_url = first_link_target.get_attribute("href")
+        assert first_url == value_for_comparison, "Неправильный сайт"
+
+
 try:
-    # проверяем наличие поля поиска
-    search_selector = ".input__control.input__input"
-    search = WebDriverWait(browser, 10).until(
-        ec.visibility_of_element_located((By.CSS_SELECTOR, search_selector))
-    )
-    ActionChains(browser).move_to_element(search).perform()
-    assert search, "Поле не найдено"
-
-    # вводим в поле поиска нужный запрос
-    request = "тензор"
-    search.send_keys(request)
-
-    # проверяем, появилась ли таблица с подсказками
-    table_selector = "div.mini-suggest__popup-content"
-    table = WebDriverWait(browser, 10).until(
-        ec.visibility_of_element_located((By.CSS_SELECTOR, table_selector))
-    )
-    assert table, "Таблица не найдена"
-
-    # находим кнопку "Найти" и жмём на неё
-    find_button_selector = ".button.mini-suggest__button"
-    do_action(find_button_selector)
-
-    # проверяем, ведёт ли первая ссылка на сайт tensor.ru
-    first_link_selector = "#search-result > li:nth-child(3) > div > h2 > a"
-    first_link_target = WebDriverWait(browser, 10).until(
-        ec.visibility_of_element_located((By.CSS_SELECTOR, first_link_selector))
-    )
-    first_url = first_link_target.get_attribute("href")
-    compared_value = "https://tensor.ru/"
-    assert first_url == compared_value, "Неправильный сайт"
-
+    if __name__ == "__main__":
+        # создаём экземпляр класса
+        test = TestMainPage()
+        # проверяем наличие поля поиска
+        test.test_find_search(search_selector, request)
+        # проверяем, появилась ли таблица с подсказками
+        test.test_table(table_selector)
+        # находим кнопку "Найти" и жмём на неё
+        do_action(find_button_selector)
+        # проверяем, ведёт ли первая ссылка поиска на сайт tensor.ru
+        test.test_first_link(first_link_selector, compared_value)
+        # если все тесты пройдены успешно, выводим на экран сообщение
+        print("All tests passed!")
 finally:
     browser.quit()
--- test_2.py
+++ test_2.py
@@ -4,18 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+# задаём все переменные, используемые в коде
+link = "https://yandex.ru/"
+images_selector = "div.home-arrow__tabs a:nth-child(3)"
+first_img_selector = "#main > div > div > div:nth-child(1) > div:nth-child(1) > div > a"
+img_selector = "div.cl-viewer-image img"
+forward_selector = "div.cl-viewer-navigate__item_right"
+back_selector = "div.cl-viewer-navigate__item_left"
+
 # инициализируем драйвер браузера
 browser = webdriver.Chrome()
 browser.maximize_window()
+browser.implicitly_wait(15)
 
-# заходим на нужный сайт по ссылке link
-link = "https://yandex.ru/"
-browser.get(link)
-browser.implicitly_wait(5)
+# помещаем тестовые сценарии в функции
 
 
 def do_action(target):
-    target_element = WebDriverWait(browser, 10).until(
+    target_element = WebDriverWait(browser, 15).until(
         ec.visibility_of_element_located((By.CSS_SELECTOR, target))
     )
     target_click = browser.find_element_by_css_selector(target)
@@ -26,51 +32,68 @@ def do_action(target):
     return target_element
 
 
+class TestMainPage1(object):
+
+    def __init__(self):
+        pass
+
+    def test_find_images_link(self, imgs_selector):
+        browser.get(link)
+        images_link = browser.find_element_by_css_selector(imgs_selector)
+        images_link = images_link.get_attribute("href")
+        assert images_link, "Ссылка не найдена"
+        browser.get(images_link)
+
+    def test_find_first_image(self, any_img_selector):
+        img = WebDriverWait(browser, 15).until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, any_img_selector))
+        )
+        first_compared_img = img.get_attribute("src")
+        assert img, "Картинка не найдена"
+        return first_compared_img
+
+    def test_forward_button_exist(self, button_selector):
+        assert do_action(button_selector), "Нет кнопки 'вперёд'"
+
+    def test_back_button_exist(self, button_selector):
+        assert do_action(button_selector), "Нет кнопки 'назад'"
+
+    def test_change_img(self, any_img_selector, comp_img):
+        second_img = WebDriverWait(browser, 15).until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, any_img_selector))
+        )
+        second_compared_img = second_img.get_attribute("src")
+        assert comp_img != second_compared_img, "Изображение не изменилось"
+
+    def test_return_to_first_img(self, any_img_selector, comp_img):
+        source_img = WebDriverWait(browser, 15).until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, any_img_selector))
+        )
+        source_img_comp = source_img.get_attribute("src")
+        assert comp_img == source_img_comp, "Изображение не вернулось к исходному"
+
+
 try:
-    # проверяем на наличие ссылку "Картинки"
-    images_selector = "div.home-arrow__tabs a:nth-child(3)"
-    images_link = browser.find_element_by_css_selector(images_selector)
-    images_link = images_link.get_attribute("href")
-    assert images_link, "Ссылка не найдена"
-    browser.get(images_link)
-
-    # кликаем на первую картинку
-    first_img_selector = "#main > div > div > div:nth-child(1) > div:nth-child(1) > div > a"
-    do_action(first_img_selector)
-
-    # проверяем, открылась ли картинка
-    img_selector = "div.cl-viewer-image img"
-    img = WebDriverWait(browser, 10).until(
-        ec.visibility_of_element_located((By.CSS_SELECTOR, img_selector))
-    )
-    assert img, "Картинка не найдена"
-
-    # достаем ссылку первого изображения для дальнейшего сравнения
-    first_compared_img = img.get_attribute("src")
-
-    # создаём CSS-селекторы для кнопок "вперёд" и "назад"
-    forward_selector = "div.cl-viewer-navigate__item_right"
-    back_selector = "div.cl-viewer-navigate__item_left"
-
-    # проверяем наличие и кликаем на кнопку "вперёд"
-    assert do_action(forward_selector), "Нет кнопки 'вперёд'"
-
-    # проверяем, изменилась ли картинка
-    second_img = WebDriverWait(browser, 10).until(
-        ec.visibility_of_element_located((By.CSS_SELECTOR, img_selector))
-    )
-    second_compared_img = second_img.get_attribute("src")
-    assert first_compared_img != second_compared_img, "Изображение не изменилось"
-
-    # проверяем наличие и кликаем на кнопку "назад"
-    assert do_action(back_selector), "Нет кнопки 'назад'"
-
-    # проверяем, вернулись ли мы к исходной картинке
-    source_img = WebDriverWait(browser, 10).until(
-        ec.visibility_of_element_located((By.CSS_SELECTOR, img_selector))
-    )
-    source_img_comp = source_img.get_attribute("src")
-    assert first_compared_img == source_img_comp, "Изображение не вернулось к исходному"
+    if __name__ == "__main__":
+        # создаём экземпляр класса
+        test = TestMainPage1()
+        # проверяем на наличие ссылку "Картинки"
+        test.test_find_images_link(images_selector)
+        # кликаем на первую картинку
+        do_action(first_img_selector)
+        # проверяем, открылась ли картинка и
+        # достаем ссылку первого изображения для дальнейшего сравнения
+        compared_img = test.test_find_first_image(img_selector)
+        # проверяем наличие и кликаем на кнопку "вперёд"
+        test.test_forward_button_exist(forward_selector)
+        # проверяем, изменилась ли картинка
+        test.test_change_img(img_selector, compared_img)
+        # проверяем на наличие и кликаем на кнопку "назад"
+        test.test_back_button_exist(back_selector)
+        # проверяем, вернулись ли мы к исходному изображению
+        test.test_return_to_first_img(img_selector, compared_img)
+        # если все тесты пройдены успешно, выводим на экран сообщение
+        print("All tests passed!")
 
 finally:
     browser.quit()
